12ddb2f7769c203b835cb184246c357c6be5b063
lzlhero <lzlhero@gmail.com>
2019-11-01 09:00:42 +0800

    comments /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/usr/local/bin/node/bin:/home/pi/tools/shell printing

--- index.js
+++ index.js
@@ -279,9 +279,11 @@ var server = http.createServer()
 })
 .listen(8080, '0.0.0.0', function() {
 	console.log('Http(s) proxy ' + (allBySocks ? 'by socks ' : '' ) + 'service on ' + this.address().address + ':' + this.address().port);
+	/*
 	shell.exec('echo $PATH', function(err, stdout, stderr) {
 		consoleLog('$PATH: ' + stdout.trim());
 	});
+	*/
 });
 
 // important, set inactivity http timeout
