adc607650b548f90d829df585bd2e3df767d384c
lzlhero <lzlhero@gmail.com>
2025-02-23 14:06:18 +0800

    fixed socket destroy

--- http-proxy.js
+++ http-proxy.js
@@ -39,9 +39,10 @@ function isValidHttpUrl(url) {
 // destroy socket resources
 function destroySocket() {
   for (var i = 0; i < arguments.length; i++) {
-    if (!arguments[i].destroyed) {
-      arguments[i].destroySoon();
+    if (arguments[i].writable) {
+      arguments[i].end();
     }
+    arguments[i].destroy();
   }
 }
 
