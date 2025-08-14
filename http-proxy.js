2c18b840727f36f1220585609413e9643375f588
lzlhero <lzlhero@gmail.com>
2025-08-14 12:06:20 +0800

    add 'socks-proxy' log and domains

--- http-proxy.js
+++ http-proxy.js
@@ -52,9 +52,8 @@ function destroySocket() {
 // restart http proxy by shell script
 function restartHttpProxy() {
   shell.exec('http-proxy', function(err, stdout, stderr) {
-    if(err) {
-      console.log(`http-proxy error: ${stderr.trim()}`);
-    }
+    // only be executed with http-proxy failure
+    console.log(`http-proxy: ${stderr.trim()}`);
   });
 }
 
@@ -62,9 +61,7 @@ function restartHttpProxy() {
 // restart socks proxy by shell script
 function restartSocksProxy() {
   shell.exec('socks-proxy', function(err, stdout, stderr) {
-    if(err) {
-      console.log(`socks-proxy error: ${stderr.trim()}`);
-    }
+    console.log(`socks-proxy: ${err ? stderr.trim() : 'executed.'}`);
   });
 }
 
